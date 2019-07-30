4cacf58199a2d494a47061dde53b201684d34da7
renan-dev <renanprch@gmail.com>
2019-07-30 09:07:13 -0300

    Criar Procfile e importar heroku

---
+++ ambiente/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\renomear.desktop-bs0u13s\desktop\d402-sistema-banco\ambiente\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
