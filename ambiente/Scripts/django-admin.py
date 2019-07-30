6f4eaef65c2418b40f78deff594826342b72e2e5
Renan Rocha <39072875+renan-dev@users.noreply.github.com>
2019-07-30 09:29:14 -0300

    Delete django-admin.py

--- ambiente/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\renomear.desktop-bs0u13s\desktop\d402-sistema-banco\ambiente\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
